06aaa2ef2872ab43be697c19514eabebac543def
Ritchie Martori <skawful@gmail.com>
2015-01-22 16:33:57 -0800

    Integrate with load-balancer

--- proxy/boot/load-balancer.js
+++ proxy/boot/load-balancer.js
@@ -26,6 +26,26 @@ module.exports = function(server) {
 
   LoadBalancer.prototype.updateConfig = function(hosts, cb) {
     // call remote method with hosts...
-    cb();
+    this.createClient().models.Config.setEndpoints(hosts, cb);
+  }
+
+  LoadBalancer.prototype.createClient = function() {
+    var host = this;
+    var client = loopback();
+    var ds = client.dataSource('remote', {
+      connector: 'remote',
+      url: this.toURL() + '/api'
+    });
+
+    // TODO(ritch) this is going to block and be incredibly slow...
+    boot(client, {
+      appRootDir: path.join(path.dirname(require.resolve('strong-arc-lb')))
+    });
+
+    return client;
+  }
+
+  LoadBalancer.prototype.toURL = function() {
+    return this.protocol + '://' + this.host + ':' + this.port;
   }
 }
